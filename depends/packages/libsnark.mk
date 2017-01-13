1467ba7ca15030dda6c3e778b637b5321d0dc292
Joshua Yabut <yabut.joshua@gmail.com>
2017-01-12 20:44:51 -0500

    V1.0.4 mac (#51)
    * initial mac version of zclassic
    
    Work in progress - 15JAN2017
    
    more refactoring
    
    linux refactoring fixes
    
    osx refactoring fixes
    
    initial win64 commit
    
    fixup! initial win64 commit
    
    compile libsnark with posix threads
    
    build gtest and gmock with posix
    
    Working build
    
    fixup! Working build

--- depends/packages/libsnark.mk
+++ depends/packages/libsnark.mk
@@ -1,15 +1,15 @@
 package=libsnark
-$(package)_version=0.1
-$(package)_download_path=https://github.com/zcash/$(package)/archive/
+$(package)_download_path=https://github.com/z-classic/$(package)/archive/
 $(package)_file_name=$(package)-$($(package)_git_commit).tar.gz
 $(package)_download_file=$($(package)_git_commit).tar.gz
-$(package)_sha256_hash=9422b1a2a94e6b8be61f07af7f146087c2a7d70b208d07ad076622225aa7f0e4
-$(package)_git_commit=2e6314a9f7efcd9af1c77669d7d9a229df86a777
+
+$(package)_sha256_hash=2a8712a575bd9a98c1d1b2a1ebbded554cef09eea311c58da7cbc1278c306964
+$(package)_git_commit=55e431e0117c7eea01f878244a664ad3554ea1de
 
 $(package)_dependencies=libgmp libsodium
 
 define $(package)_build_cmds
-  CXXFLAGS="-fPIC -DBINARY_OUTPUT -DNO_PT_COMPRESSION=1" $(MAKE) lib DEPINST=$(host_prefix) CURVE=ALT_BN128 MULTICORE=1 NO_PROCPS=1 NO_GTEST=1 NO_DOCS=1 STATIC=1 NO_SUPERCOP=1 FEATUREFLAGS=-DMONTGOMERY_OUTPUT OPTFLAGS="-O2 -march=x86-64"
+  CXX="x86_64-w64-mingw32-g++-posix" CXXFLAGS="-DBINARY_OUTPUT -DPTW32_STATIC_LIB -DSTATICLIB -DNO_PT_COMPRESSION=1 -fopenmp" $(MAKE) lib DEPINST=$(host_prefix) CURVE=ALT_BN128 MULTICORE=1 NO_PROCPS=1 NO_GTEST=1 NO_DOCS=1 STATIC=1 NO_SUPERCOP=1 FEATUREFLAGS=-DMONTGOMERY_OUTPUT OPTFLAGS="-O2 -march=x86-64"
 endef
 
 define $(package)_stage_cmds
