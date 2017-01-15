f1bf96ab24baad9d0029e427db556f75f1e1b449
joshuayabut <yabut.joshua@gmail.com>
2017-01-15 03:57:00 -0500

    Work in progress - 15JAN2017

--- depends/packages/libsnark.mk
+++ depends/packages/libsnark.mk
@@ -1,15 +1,15 @@
 package=libsnark
-$(package)_version=0.1
-$(package)_download_path=https://github.com/radix42/$(package)/archive/
+$(package)_download_path=https://github.com/z-classic/$(package)/archive/
 $(package)_file_name=$(package)-$($(package)_git_commit).tar.gz
 $(package)_download_file=$($(package)_git_commit).tar.gz
-$(package)_sha256_hash=9dbd5b44d3443e86463e934bfe1023cab4ca5948f8d74c23a67d9535c28d2584
-$(package)_git_commit=9be18569b8abcda1245c3912877075259599c0f1
+
+$(package)_sha256_hash=2a8712a575bd9a98c1d1b2a1ebbded554cef09eea311c58da7cbc1278c306964
+$(package)_git_commit=55e431e0117c7eea01f878244a664ad3554ea1de
 
 $(package)_dependencies=libgmp libsodium
 
 define $(package)_build_cmds
-CC=gcc-5 CXX=g++-5 CXXFLAGS="-arch x86_64 -DBINARY_OUTPUT -DNO_PT_COMPRESSION=1" $(MAKE) lib DEPINST=$(host_prefix) CURVE=ALT_BN128 MULTICORE=0 NO_PROCPS=1 NO_GTEST=1 NO_DOCS=1 STATIC=1 NO_SUPERCOP=1 FEATUREFLAGS=-DMONTGOMERY_OUTPUT
+CC=gcc-5 CXX=g++-5 CXXFLAGS="-arch x86_64 -fPIC -DBINARY_OUTPUT -DNO_PT_COMPRESSION=1" $(MAKE) lib DEPINST=$(host_prefix) CURVE=ALT_BN128 MULTICORE=1 NO_PROCPS=1 NO_GTEST=1 NO_DOCS=1 STATIC=1 NO_SUPERCOP=1 FEATUREFLAGS=-DMONTGOMERY_OUTPUT
 endef
 
 define $(package)_stage_cmds
--- zcutil/fetch-params.sh
+++ zcutil/fetch-params.sh
@@ -2,12 +2,16 @@
 
 set -eu
 
-PARAMS_DIR="$HOME/Library/Application Support/ZcashParams"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    PARAMS_DIR="$HOME/Library/Application Support/ZcashParams"
+else
+    PARAMS_DIR="$HOME/.zcash-params"
+fi
 
 SPROUT_PKEY_NAME='sprout-proving.key'
 SPROUT_VKEY_NAME='sprout-verifying.key'
-SPROUT_PKEY_URL="https://zcash.dl.mercerweiss.com/$SPROUT_PKEY_NAME"
-SPROUT_VKEY_URL="https://zcash.dl.mercerweiss.com/$SPROUT_VKEY_NAME"
+SPROUT_PKEY_URL="https://z.cash/downloads/$SPROUT_PKEY_NAME"
+SPROUT_VKEY_URL="https://z.cash/downloads/$SPROUT_VKEY_NAME"
 
 SHA256CMD="$(command -v sha256sum || echo shasum)"
 SHA256ARGS="$(command -v sha256sum >/dev/null || echo '-a 256')"
@@ -18,13 +22,21 @@ function fetch_params {
     local dlname="${output}.dl"
     local expectedhash="$3"
 
-    if ! [ -f "$output" ]
-    then
+    if ! [ -f "$output" ]; then
         echo "Retrieving: $url"
-        curl \
-            --output "$dlname" \
-            -# -L -C - \
-            "$url"
+        if [ $(sw_vers -productName) == "Mac" ]; then
+            curl \
+                --output "$dlname" \
+                -# -L -C - \
+                "$url"
+        else
+            wget \
+                --progress=dot:giga \
+                --output-document="$dlname" \
+                --continue \
+                --retry-connrefused --waitretry=3 --timeout=30 \
+                "$url"
+        fi
 
         "$SHA256CMD" $SHA256ARGS --check <<EOF
 $expectedhash  $dlname
