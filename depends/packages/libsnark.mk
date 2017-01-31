f304d4a6e990f98acfc91bcc8d64a02ff90b8505
joshuayabut <yabut.joshua@gmail.com>
2017-01-30 19:26:58 -0500

    Windows and Linux builds ok

--- depends/packages/libsnark.mk
+++ depends/packages/libsnark.mk
@@ -3,15 +3,20 @@ $(package)_download_path=https://github.com/z-classic/$(package)/archive/
 $(package)_file_name=$(package)-$($(package)_git_commit).tar.gz
 $(package)_download_file=$($(package)_git_commit).tar.gz
 
-$(package)_sha256_hash=2a8712a575bd9a98c1d1b2a1ebbded554cef09eea311c58da7cbc1278c306964
-$(package)_git_commit=55e431e0117c7eea01f878244a664ad3554ea1de
+$(package)_sha256_hash=f23624c4e6f49c8e7989673f06b6ccadd5e2772ef5d5d378728e4a1ff8a6f260
+$(package)_git_commit=f646a4967f7c987232aeb6e7bad95ede7cb1a740
+
+define $(package)_set_vars
+    $(package)_build_env=CC="$($(package)_cc)" CXX="$($(package)_cxx)"
+    $(package)_build_env+=CXXFLAGS="$($(package)_cxxflags) -DBINARY_OUTPUT -DSTATICLIB -DNO_PT_COMPRESSION=1 "
+endef
 
 $(package)_dependencies=libgmp libsodium
 
 define $(package)_build_cmds
-  CXX="x86_64-w64-mingw32-g++-posix" CXXFLAGS="-DBINARY_OUTPUT -DPTW32_STATIC_LIB -DSTATICLIB -DNO_PT_COMPRESSION=1 -fopenmp" $(MAKE) lib DEPINST=$(host_prefix) CURVE=ALT_BN128 MULTICORE=1 NO_PROCPS=1 NO_GTEST=1 NO_DOCS=1 STATIC=1 NO_SUPERCOP=1 FEATUREFLAGS=-DMONTGOMERY_OUTPUT OPTFLAGS="-O2 -march=x86-64"
+    $(MAKE) lib DEPINST=$(host_prefix) CURVE=ALT_BN128 MULTICORE=1 NO_PROCPS=1 NO_GTEST=1 NO_DOCS=1 STATIC=1 NO_SUPERCOP=1 FEATUREFLAGS=-DMONTGOMERY_OUTPUT OPTFLAGS="-O2 -march=x86-64"
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) install STATIC=1 DEPINST=$(host_prefix) PREFIX=$($(package)_staging_dir)$(host_prefix) CURVE=ALT_BN128 NO_SUPERCOP=1
+    $(MAKE) install STATIC=1 DEPINST=$(host_prefix) PREFIX=$($(package)_staging_dir)$(host_prefix) CURVE=ALT_BN128 NO_SUPERCOP=1
 endef
--- zcutil/build-win.sh
+++ zcutil/build-win.sh
@@ -11,6 +11,6 @@ cd "$(dirname "$(readlink -f "$0")")/.."
 
 cd depends/ && make HOST=$HOST V=1 NO_QT=1 && cd ../
 ./autogen.sh
-CONFIG_SITE=$PWD/depends/x86_64-w64-mingw32/share/config.site CXXFLAGS+=" -fopenmp" ./configure --prefix="${PREFIX}" --with-gui=no --host="${HOST}" --enable-static --disable-shared
+CXXFLAGS="-DPTW32_STATIC_LIB -DCURVE_ALT_BN128 -fopenmp -pthread" CONFIG_SITE=$PWD/depends/x86_64-w64-mingw32/share/config.site ./configure --prefix="${PREFIX}" --with-gui=no --host="${HOST}" --enable-static --disable-shared
 sed -i 's/-lboost_system-mt /-lboost_system-mt-s /' configure
 CC="${CC}" CXX="${CXX}" make V=1
